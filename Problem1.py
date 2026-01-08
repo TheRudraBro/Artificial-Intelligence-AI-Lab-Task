e1c791de364199dc92069cf381ac1fbe150756fb
Rudra MojumDer <rudramraj22@gmail.com>
2026-01-08 23:52:24 +0600

    Add age-based voter message logic in Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,8 @@
+age = int (input("age"))
+
+if age < 18:
+    print('Voter Na')
+elif age > 18 and age < 40:
+    print('Kare vote dibo confused unga bunga')
+elif age>40:
+    print('Buira hooi gelam')
